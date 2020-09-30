597dd33a13bd49e986696a966febc2da978260fc
Jesse Parsons <savvyjesse@aol.com>
2020-09-30 19:57:36 -0400

    generate icons for removing book object from page upon mouse event, add listener to toggle read status of book

--- library/script.js
+++ library/script.js
@@ -1,17 +1,17 @@
 let myLibrary = [];
+const bookContainer = document.getElementById("book-container");
 const addBookButton = document.getElementById("add-book");
 addBookButton.addEventListener('click', function() {
     const authorName = document.getElementById("author");
     const title = document.getElementById("title");
     const numberOfPages = document.getElementById("pages");
     const read = document.getElementById("read").checked;
-    console.log(read);
 
     if (validateBook(numberOfPages.value, authorName.value, title.value)) {
         $("#book-form").modal("toggle");
         //create book object and add to page
-        //new book objs index will be myLibrary.length - 1
         const book = new Book(title.value, authorName.value, numberOfPages.value, read);
+        bookContainer.style = "display: flex;"
         addBookToLibrary(book);
         appendToDocument(book, (myLibrary.length - 1));
         //clear form
@@ -51,17 +51,17 @@ function addBookToLibrary(book) {
 
 function appendToDocument(bookObj, index) {
     const bookContainer = document.getElementsByClassName("book-container")[0];
-
     //book root
     let bookElement = document.createElement("div");
-    bookElement.classList = "card book col-sm-12 col-md-4 col-lg-4"
+    bookElement.appendChild(generateIcon(index));
 
+
+    bookElement.classList = "card book col-sm-12 col-md-4 col-lg-4"
     //book img thumbnail
     let bookImg = document.createElement("img");
     bookImg.classList = "card-img-top thumbnail img-fluid";
     bookImg.src = "./assets/images/eloquent-javascript.jpg";
     let link = document.createElement("a");
-    console.log(bookObj.title.split(" "))
     //construct query string for search engine link
     let queryString = "q=";
     bookObj.title.split(" ").forEach(element => {
@@ -93,12 +93,48 @@ function appendToDocument(bookObj, index) {
             readStatus.innerHTML = "Unread"
         }
         bookInfo.appendChild(readStatus);
-    bookElement.appendChild(bookInfo);
-    bookElement.setAttribute('data-index', index);
-    bookContainer.appendChild(bookElement);
+        bookElement.appendChild(bookInfo);
+        bookElement.setAttribute('data-index', index);
+        bookContainer.appendChild(bookElement);
+        //add listener for toggling read status and changing read attribute in associated object
+        readStatus.addEventListener("click", function() {
+            let index = this.parentNode.parentNode.dataset.index;
+            if (this.classList == "read") {
+                this.classList = "unread";
+                this.innerHTML = "Unread";
+                myLibrary[index].read = false
+            } else {
+                this.classList = "read";
+                this.innerHTML = "Read";
+                myLibrary[index].read = true;
+            }
+        })
 }
 
-// addBookToLibrary(new Book("Homeland", "R.A. Salvatore", "352", true));
-// addBookToLibrary(new Book("Eloquent Javascript, 3rd Edition: A Modern Introduction to Programming", "Marijn Haverbeke", 472, false));
-// addBookToLibrary(new Book("The Fellowship of the Ring", "J.R.R. Tolkien", 423, true))
-// myLibrary.forEach(appendToDocument);
+function generateIcon(index) {
+    let iconElement = document.createElement("i");
+    iconElement.classList = "fas fa-times-circle fa-3x remove-book";
+    iconElement.style = "position: absolute; margin-left: 2em; margin-bottom: 1.5em; top: 0; right: 0;";
+    iconElement.setAttribute("data-index", index)
+    addListener(iconElement);
+    return iconElement;
+}
+
+function addListener(element) {
+    element.addEventListener("click", function() {
+        let parent = element.parentNode
+        let parentRoot = parent.parentNode
+        element.parentNode.removeChild(element)
+        parentRoot.removeChild(parent);
+        checkEmpty();
+        //keep books in library for now
+        // myLibrary.splice(element.dataset.index, 1);
+    })
+}
+// hide container and ugly empty element if there are no books
+function checkEmpty() {
+    if (bookContainer.childElementCount == 0) {
+        bookContainer.style = "display: none;"
+    }
+}
+checkEmpty();
