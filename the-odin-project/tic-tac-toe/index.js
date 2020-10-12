72cd0b97baffee5123b060cc167bc1661b3b5eb8
Jesse Parsons <savvyjesse@aol.com>
2020-10-12 12:49:48 -0400

    add module for basic game functionality

--- the-odin-project/tic-tac-toe/index.js
+++ the-odin-project/tic-tac-toe/index.js
@@ -1,5 +1,25 @@
-const cells = document.querySelectorAll('.cell');
-
+const cells = document.querySelectorAll('.cell'); // polluting global namespace
+const game = function() {
+    const board = Array(9).fill("");
+    let xIsNext = true;
+    function renderBoard() {
+        cells.forEach(function(el, index) {
+            el.innerHTML = board[index];
+        });
+    }
+    function updateCell() {
+        console.log("in update cell method");
+        console.log(this);
+        this.innerHTML = xIsNext ? "X" : "O";
+        xIsNext = false;
+    }
+    return {
+        renderBoard,
+        xIsNext,
+        board,
+        updateCell,
+    }
+}();
 cells.forEach(element => {
-    element.addEventListener('click', () => console.log('clicked ' + element.id))
+    element.addEventListener('click', game.updateCell.bind(element))
 });
