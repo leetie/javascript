8f75fbb82d3faf15b10f1bfbf600213258a7b019
Jesse Parsons <savvyjesse@aol.com>
2020-10-18 13:47:12 -0400

    add unique ids to book objects using Date.now(), modify listener and data attrs on html elements to be able to reliably remove book objects regardless of current state of books array

--- the-odin-project/library/script_with_classes.js
+++ the-odin-project/library/script_with_classes.js
@@ -10,6 +10,7 @@ class Book {
         this.author = author;
         this.numPages = numPages;
         this.read = read;
+        this.id = Date.now();
     }
     validateBook() {
         if (this.title != "" && this.author != "" && this.numPages > 0) {
@@ -18,6 +19,58 @@ class Book {
             return false
         }
     }
+    render() {
+        const bookContainer = document.getElementsByClassName("book-container")[0];
+        //book root
+        let bookElement = document.createElement("div");
+        bookElement.appendChild(generateIcon(this.id)); //###### UUID HERE
+        bookElement.classList = "card book col-sm-12 col-md-4 col-lg-4";
+        //book img thumbnail
+        let bookImg = document.createElement("img");
+        bookImg.classList = "card-img-top thumbnail img-fluid";
+        bookImg.src = "./assets/images/eloquent-javascript.jpg";
+        //book information
+        let bookInfo = document.createElement("div");
+        bookInfo.classList = "card-body";
+        let bookTitleAndAuthor = document.createElement("h5");
+            bookTitleAndAuthor.classList = "card-title"
+            bookTitleAndAuthor.innerHTML = this.title + ", " + "by " + this.author
+            bookInfo.appendChild(bookTitleAndAuthor);
+        let bookPages = document.createElement("p");
+            bookPages.classList = "pages";
+            bookPages.innerHTML = this.numPages + " Pages - ";
+            bookInfo.appendChild(bookPages);
+        let readStatus = document.createElement("div");
+        if (this.read == true) {
+            readStatus.classList = "read";
+            readStatus.innerHTML = "Read";
+        } else {
+            readStatus.classList = "unread";
+            readStatus.innerHTML = "Unread"
+        }
+        bookInfo.appendChild(readStatus);
+        bookElement.appendChild(bookInfo);
+        bookElement.setAttribute('data-id', this.id); //######UUID HERE
+        bookContainer.appendChild(bookElement);
+        console.log("book rendered!");
+    }
+}
+// class for icon element???
+function generateIcon(id) { //##### UUID HERE
+    let iconElement = document.createElement("i");
+    iconElement.classList = "fas fa-times-circle fa-3x remove-book";
+    iconElement.style = "position: absolute; margin-left: 2em; margin-bottom: 1.5em; top: 0; right: 0;";
+    iconElement.setAttribute("data-id", id)
+    iconElement.addEventListener('click', function() {
+        let bookToRemove = myLibrary.books.find(function(item) {
+            return item.id == iconElement.id;
+        });
+        myLibrary.books.splice(
+            myLibrary.books.indexOf(bookToRemove),
+            1
+        );
+    });
+    return iconElement;
 }
 
 class Library {
@@ -30,13 +83,19 @@ class Library {
     }
 
     render() {
+        // clear and re-render page on library render -- else 
+        // all book objects are rendered twice
         console.log('render some stuff to document')
         console.log(this.books)
+        this.books.forEach(function(book, index) {
+            book.render(index);
+        });
     }
 }
 
 let myLibrary = new Library();
 
+// grab info from form, create book obj, then clear form
 addBookButton.addEventListener('click', function() {
     const authorName = document.getElementById("author");
     const title = document.getElementById("title");
